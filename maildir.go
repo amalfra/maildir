facbf1983610fbc108a26a071be8a848ba9e0e60
Amal Francis <amalfra@gmail.com>
2017-11-30 12:36:14 +0530

    add maildir API package bootstrap code

---
+++ maildir.go
@@ -0,0 +1,29 @@
+package maildir
+
+import (
+	"maildir/lib"
+	"os"
+	"path"
+)
+
+// Maildir implements maildir format and it's operations
+type Maildir struct {
+	path string
+}
+
+// NewMaildir will create new maildir at specified path
+func NewMaildir(path string, create bool) *Maildir {
+	maildir := new(Maildir)
+	maildir.path = path
+	if create {
+		maildir.createDirectories()
+	}
+	return maildir
+}
+
+// createDirectories will the sub directories required by maildir
+func (m *Maildir) createDirectories() {
+	for i := 0; i < len(lib.Subdirs); i++ {
+		os.MkdirAll(path.Join(m.path, lib.Subdirs[i]), os.ModePerm)
+	}
+}
---
+++ maildir_test.go
@@ -0,0 +1,43 @@
+package maildir
+
+import (
+	"fmt"
+	"maildir/lib"
+	"os"
+	"path"
+	"testing"
+)
+
+var mailDir string
+var testData string
+var maildir *Maildir
+
+func cleanMaildir() {
+	err := os.RemoveAll(mailDir)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to clean maildir folder")
+		os.Exit(1)
+	}
+}
+
+func init() {
+	mailDir = "/tmp/maildir-test"
+	testData = "foo"
+	cleanMaildir()
+
+	maildir = NewMaildir(mailDir, true)
+}
+
+func TestPathProperty(t *testing.T) {
+	if maildir.path != mailDir {
+		t.Fatalf("path mismatch")
+	}
+}
+
+func TestDirectoriesCreated(t *testing.T) {
+	for i := 0; i < len(lib.Subdirs); i++ {
+		if _, err := os.Stat(path.Join(mailDir, lib.Subdirs[i])); os.IsNotExist(err) {
+			t.Fatalf("required sub directories not created")
+		}
+	}
+}
